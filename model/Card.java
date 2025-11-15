f9344b427ec5e172df79ecc5b7ebb01a787a7a96
vincentzan32 <vzhang49@bu.edu>
2025-11-15 01:53:11 -0500

    created the card class which takes both the Rank and Suit of the card and put its into one object

---
+++ model/Card.java
@@ -0,0 +1,28 @@
+package model;
+
+/*
+ * Creates the physical card and everything needed for the card
+ */
+
+public class Card {
+    private final Rank rank;
+    private final Suit suit;
+
+    public Card(Rank rank, Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
